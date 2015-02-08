37a1375f70f9bcf2614a66c96345f96ca5216af6
Cyle Gage <cylegage@gmail.com>
2015-02-08 16:16:38 -0500

    adding username to registration process

--- www/register.php
+++ www/register.php
@@ -22,10 +22,11 @@
 		<form action="/register/process/" method="post">
 		<table>
 		<tr><td>Your Email:</td><td><input tabindex="1" id="start-here" name="e" type="email" placeholder="you@fuck.off" /></td></tr>
-		<tr><td>Your Password:</td><td><input tabindex="2" name="p1" type="password" /></td></tr>
-		<tr><td>Your Password:</td><td><input tabindex="3" name="p2" type="password" /></td></tr>
-		<tr><td>Invite Code:</td><td><input tabindex="4" name="i" type="password" /></td></tr>
-		<tr><td><input tabindex="5" type="submit" value="sign up &raquo;" /></td><td></td></tr>
+		<tr><td>Your Public Username:</td><td><input tabindex="2" name="u" type="text" maxlength="50" placeholder="frigger" /></td></tr>
+		<tr><td>Your Password:</td><td><input tabindex="3" name="p1" type="password" /></td></tr>
+		<tr><td>Your Password:</td><td><input tabindex="4" name="p2" type="password" /></td></tr>
+		<tr><td>Invite Code:</td><td><input tabindex="5" name="i" type="password" /></td></tr>
+		<tr><td><input tabindex="6" type="submit" value="sign up &raquo;" /></td><td></td></tr>
 		</table>
 		</form>
 	</div>
--- www/register_process.php
+++ www/register_process.php
@@ -14,6 +14,10 @@ if (!filter_var(trim($_POST['e']), FILTER_VALIDATE_EMAIL)) {
 	die('the email address you put in is invalid, jeez.');
 }
 
+if (!isset($_POST['u']) || trim($_POST['u']) == '') {
+	die('you forgot to put in a goddamn username, jeez.');
+}
+
 if (!isset($_POST['p1']) || trim($_POST['p1']) == '') {
 	die('you forgot to put in your password, jeez.');
 }
@@ -29,11 +33,18 @@ if (trim($_POST['p1']) != trim($_POST['p2'])) {
 require_once('../lib/dank/dbconn_mysql.php');
 
 $new_user_email_db = "'".$mysqli->escape_string(trim($_POST['e']))."'";
+$new_username_db = "'".$mysqli->escape_string(trim($_POST['u']))."'";
 
 // check to see if email already in use
 $check_for_email = $mysqli->query("SELECT user_id FROM users WHERE email=$new_user_email_db");
 if ($check_for_email->num_rows > 0) {
 	die('sorry, but that email address appears to already be in use.');
+}
+
+// check to see if username already in use
+$check_for_username = $mysqli->query("SELECT user_id FROM users WHERE username=$new_username_db");
+if ($check_for_username->num_rows > 0) {
+	die('sorry, but that username appears to already be in use.');
 }
 
 // check to make sure their invite code matches
@@ -50,7 +61,7 @@ $invite_row = $check_invite->fetch_assoc();
 $new_user_pwd_hash = password_hash(trim($_POST['p1']), PASSWORD_DEFAULT);
 $new_user_pwd_hash_db = "'".$mysqli->escape_string($new_user_pwd_hash)."'";
 
-$new_user_row = $mysqli->query("INSERT INTO users (email, steakonions, last_activity_ts, tsc) VALUES ($new_user_email_db, $new_user_pwd_hash_db, UNIX_TIMESTAMP(), UNIX_TIMESTAMP())");
+$new_user_row = $mysqli->query("INSERT INTO users (username, email, steakonions, last_activity_ts, tsc) VALUES ($new_username_db, $new_user_email_db, $new_user_pwd_hash_db, UNIX_TIMESTAMP(), UNIX_TIMESTAMP())");
 if (!$new_user_row) {
 	die('error creating new user: '.$mysqli->error);
 }
